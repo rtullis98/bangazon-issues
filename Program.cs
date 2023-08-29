4e573816cedc637ee0ee9c727ccfe208cb3773e6
rtullis98 <121829715+rtullis98@users.noreply.github.com>
2023-08-29 18:09:13 -0500

    some more endpoints

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 using Bangazon;
+using Bangazon.Models;
 using Microsoft.AspNetCore.Http.Json;
+using Microsoft.EntityFrameworkCore;
 using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -32,68 +34,261 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+
+//customer endpoints
+app.MapGet("/api/customers", (BangazonDbContext dbContext) =>
+{
+    var customers = dbContext.Customers.ToList();
+    return Results.Ok(customers);
+});
+
+app.MapGet("/api/customers/{id}", (int id, BangazonDbContext dbContext) =>
+{
+    var customer = dbContext.Customers.Find(id);
+    if (customer == null)
+    {
+        return Results.NotFound();
+    }
+
+    return Results.Ok(customer);
+});
+
+app.MapPost("/api/customers", (Customer customer, BangazonDbContext dbContext) =>
+{
+    dbContext.Customers.Add(customer);
+    dbContext.SaveChanges();
+    return Results.Created($"/api/customers/{customer.CustomerId}", customer);
+});
+
+
+app.MapPut("/api/customers/{id}", (int id, Customer updatedCustomer, BangazonDbContext dbContext) =>
+{
+    if (id != updatedCustomer.CustomerId)
+    {
+        return Results.BadRequest();
+    }
+
+    dbContext.Entry(updatedCustomer).State = EntityState.Modified;
+
+    try
+    {
+        dbContext.SaveChanges();
+        return Results.NoContent();
+    }
+    catch (DbUpdateConcurrencyException)
+    {
+        if (!dbContext.Customers.Any(e => e.CustomerId == id))
+        {
+            return Results.NotFound();
+        }
+
+        throw;
+    }
+});
+
+app.MapDelete("/api/customers/{id}", (int id, BangazonDbContext dbContext) =>
+{
+    var customer = dbContext.Customers.Find(id);
+    if (customer == null)
+    {
+        return Results.NotFound();
+    }
+
+    dbContext.Customers.Remove(customer);
+    dbContext.SaveChanges();
+    return Results.NoContent();
+});
+
+// order endpoints
+
+app.MapGet("/api/orders", (BangazonDbContext dbContext) =>
+{
+    var orders = dbContext.Orders.ToList();
+    return Results.Ok(orders);
+});
+
+app.MapGet("api/orders/{id}", (int id, BangazonDbContext dbContext) =>
+{
+    var order = dbContext.Orders.Find(id);
+    if (order == null)
+    {
+        return Results.NotFound();
+    }
+
+    return Results.Ok(order);
+});
+
+app.MapPost("/api/orders", (Order order, BangazonDbContext dbContext) =>
+{
+    dbContext.Orders.Add(order);
+    dbContext.SaveChanges();
+    return Results.Created($"/api/orders/{order.OrderId}", order);
+});
+
+app.MapPut("/api/orders/{id}", (int id, Order updatedOrder, BangazonDbContext dbContext) =>
+{
+    if (id != updatedOrder.OrderId)
+    {
+        return Results.BadRequest();
+    }
+
+    dbContext.Entry(updatedOrder).State = EntityState.Modified;
+
+    try
+    {
+        dbContext.SaveChanges();
+        return Results.NoContent();
+    }
+    catch (DbUpdateConcurrencyException)
+    {
+        if (!dbContext.Orders.Any(e => e.OrderId == id))
+        {
+            return Results.NotFound();
+        }
+
+        throw;
+    }
+});
+
+app.MapDelete("/api/orders/{id}", (int id, BangazonDbContext dbContext) =>
+{
+    var order = dbContext.Orders.Find(id);
+    if (order == null)
+    {
+        return Results.NotFound();
+    }
+
+    dbContext.Orders.Remove(order);
+    dbContext.SaveChanges();
+    return Results.NoContent();
+});
+
+// product endpoints
+
 app.MapGet("/api/products", (BangazonDbContext dbContext) =>
 {
-    var products = dbContext.Products
-        .Join(
-            dbContext.ProductCategories,
-            product => product.ProductCategoryId,
-            category => category.ProductCategoryId,
-            (product, category) => new
-            {
-                ProductId = product.ProductId,
-                Title = product.Title,
-                Description = product.Description,
-                QuantityAvailable = product.QuantityAvailable,
-                PricePerUnit = product.PricePerUnit,
-                SellerId = product.SellerId,
-                Seller = product.Seller.StoreName,
-                ProductCategoryId = product.ProductCategoryId,
-                ProductCategoryName = category.Name
-            })
-        .ToList();
-
-    if (products.Count == 0)
-    {
-        return Results.NotFound();
-    }
-
+    var products = dbContext.Products.ToList();
     return Results.Ok(products);
 });
 
-
-app.MapGet("/api/sellers/{sellerId}/products", (int sellerId, BangazonDbContext dbContext) =>
+app.MapGet("/api/products/{id}", (int id, BangazonDbContext dbContext) =>
 {
-    var products = dbContext.Products
-        .Where(p => p.SellerId == sellerId)
-        .Join(
-            dbContext.ProductCategories,
-            product => product.ProductCategoryId,
-            category => category.ProductCategoryId,
-            (product, category) => new
-            {
-                ProductId = product.ProductId,
-                Title = product.Title,
-                Description = product.Description,
-                QuantityAvailable = product.QuantityAvailable,
-                PricePerUnit = product.PricePerUnit,
-                SellerId = product.SellerId,
-                Seller = product.Seller.StoreName,
-                ProductCategoryId = product.ProductCategoryId,
-                ProductCategoryName = category.Name
-            })
-        .ToList();
-
-    if (products.Count == 0)
+    var product = dbContext.Products.Find(id);
+    if (product == null)
     {
         return Results.NotFound();
     }
 
-    return Results.Ok(products);
+    return Results.Ok(product);
 });
 
+app.MapPost("/api/products", (Product product, BangazonDbContext dbContext) =>
+{
+    dbContext.Products.Add(product);
+    dbContext.SaveChanges();
+    return Results.Created($"/api/products/{product.ProductId}", product);
+});
 
+app.MapPut("/api/products/{id}", (int id, Product updatedProduct, BangazonDbContext dbContext) =>
+{
+    if (id != updatedProduct.ProductId)
+    {
+        return Results.BadRequest();
+    }
 
+    dbContext.Entry(updatedProduct).State = EntityState.Modified;
+
+    try
+    {
+        dbContext.SaveChanges();
+        return Results.NoContent();
+    }
+    catch (DbUpdateConcurrencyException)
+    {
+        if (!dbContext.Products.Any(e => e.ProductId == id))
+        {
+            return Results.NotFound();
+        }
+
+        throw;
+    }
+});
+
+app.MapDelete("/api/products/{id}", (int id, BangazonDbContext dbContext) =>
+{
+    var product = dbContext.Products.Find(id);
+    if (product == null)
+    {
+        return Results.NotFound();
+    }
+
+    dbContext.Products.Remove(product);
+    dbContext.SaveChanges();
+    return Results.NoContent();
+});
+
+// seller endpoints
+app.MapGet("/api/sellers", (BangazonDbContext dbContext) =>
+{
+    var sellers = dbContext.Sellers.ToList();
+    return Results.Ok(sellers);
+});
+
+app.MapGet("/api/sellers/{id}", (int id, BangazonDbContext dbContext) =>
+{
+    var seller = dbContext.Sellers.Find(id);
+    if (seller == null)
+    {
+        return Results.NotFound();
+    }
+
+    return Results.Ok(seller);
+});
+
+app.MapPost("/api/sellers", (Seller seller, BangazonDbContext dbContext) =>
+{
+    dbContext.Sellers.Add(seller);
+    dbContext.SaveChanges();
+    return Results.Created($"/api/sellers/{seller.SellerId}", seller);
+});
+
+app.MapPut("/api/sellers/{id}", (int id, Seller updatedSeller, BangazonDbContext dbContext) =>
+{
+    if (id != updatedSeller.SellerId)
+    {
+        return Results.BadRequest();
+    }
+
+    dbContext.Entry(updatedSeller).State = EntityState.Modified;
+
+    try
+    {
+        dbContext.SaveChanges();
+        return Results.NoContent();
+    }
+    catch (DbUpdateConcurrencyException)
+    {
+        if (!dbContext.Sellers.Any(e => e.SellerId == id))
+        {
+            return Results.NotFound();
+        }
+
+        throw;
+    }
+});
+
+app.MapDelete("/api/sellers/{id}", (int id, BangazonDbContext dbContext) =>
+{
+    var seller = dbContext.Sellers.Find(id);
+    if (seller == null)
+    {
+        return Results.NotFound();
+    }
+
+    dbContext.Sellers.Remove(seller);
+    dbContext.SaveChanges();
+    return Results.NoContent();
+});
 
 
 app.Run();
